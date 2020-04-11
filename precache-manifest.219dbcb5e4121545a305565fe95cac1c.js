c32ad1eccb0ec6e7114b2d05dee8c1acd69479b4
Hahnbee Lee <hl985@cornell.edu>
2020-04-11 15:02:50 -0400

    Updates

---
+++ precache-manifest.219dbcb5e4121545a305565fe95cac1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f5cb2d169fccff042df2f5686378f1",
+    "url": "/admin-site/index.html"
+  },
+  {
+    "revision": "da7453e6199765a9ce2c",
+    "url": "/admin-site/static/css/main.71c498ed.chunk.css"
+  },
+  {
+    "revision": "52cf3d1d4e407330c715",
+    "url": "/admin-site/static/js/2.93d89ff3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/admin-site/static/js/2.93d89ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da7453e6199765a9ce2c",
+    "url": "/admin-site/static/js/main.74bede39.chunk.js"
+  },
+  {
+    "revision": "90d03258475d213a3ec6",
+    "url": "/admin-site/static/js/runtime-main.78ba7bfd.js"
+  }
+]);
